c9c3bcfbf7c3726dc10c0342145e675f632fed60
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-10 15:11:16 +0530

    an-ecommerce-app

---
+++ an-ecommerce-startup/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const userRouter = require('./router/userRoute');
+const productRouter = require('./router/productRoute');
+const cartRouter = require('./router/cartRoute');
+
+app.use('/users',userRouter);
+app.use('/products',productRouter);
+app.use('/cart',cartRouter);
+
+app.listen(port,(req,res)=>{
+    console.log("Server is running on http://localhost:3000");
+})
