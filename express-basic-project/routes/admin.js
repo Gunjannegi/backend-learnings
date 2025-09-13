451baf90bf3af0a5f21ed28fa6824d961e2a4a1e
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-13 15:04:35 +0530

    routes

---
+++ express-basic-project/routes/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/add-product',(req,res)=>{
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><br/><input type="number" name="size"/><br/><button type="submit">Add Product</button></form>')
+});
+
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+module.exports = router;
