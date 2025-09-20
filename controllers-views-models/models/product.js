5f87f95c669705ffc128dca9a5620ff2e0d78142
Gunjannegi <gunjan.negi.5249@gmail.com>
2025-09-20 14:41:35 +0530

    Writing and reading the file in models

---
+++ controllers-views-models/models/product.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+function getProductFromFile(cb) {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                if (err) {
+                    console.log(err)
+
+                }
+            })
+        })
+    }
+
+    static fetchAll(cb) {
+        getProductFromFile(cb)
+    }
+}
