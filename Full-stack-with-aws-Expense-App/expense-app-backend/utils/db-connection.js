2c56ac729c484b05993cae4193213c2c12a2d1e9
Gunjannegi <gunjan.negi.5249@gmail.com>
2026-03-22 17:07:34 +0530

    updated PORT

--- Full-stack-with-aws-Expense-App/expense-app-backend/utils/db-connection.js
+++ Full-stack-with-aws-Expense-App/expense-app-backend/utils/db-connection.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.RDS_ENDPOINT,  // ✅ Fixed!
     dialect: 'mysql',
-    port: process.env.PORT
+    port: process.env.DB_PORT
 });
 
 sequelize.authenticate().then(()=>{
